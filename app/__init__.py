4e1939909a4d2bfd2f9c39ec30d89609560f7362
dewanshshekhar <dewanshshekharsingh@gmail.com>
2026-05-04 18:11:15 +0530

    feat: Initialize Flask application and WSGI entry points

---
+++ app/__init__.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+db = SQLAlchemy()
+ma = Marshmallow()
+migrate = Migrate()
+cors = CORS()
+
+
+def create_app(options={}):
+    if 'TESTING' in options and options['TESTING']:
+        config_name = 'app.config.TestingConfig'
+    else:
+        config_name = os.getenv(
+            'APP_SETTINGS',
+            'app.config.DevelopmentConfig'
+        )
+    app = Flask(__name__)
+    app.config.from_object(config_name)
+    init_extensions(app)
+    register_blueprints(app)
+    return app
+
+
+def init_extensions(app):
+    db.init_app(app)
+    ma.init_app(app)
+    migrate.init_app(app, db)
+    cors.init_app(app)
+
+
+def register_blueprints(app):
+    from app.restapi import api
+    app.register_blueprint(api, url_prefix='/api')
